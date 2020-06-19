8c329866057c86122d06e51b2302d4b158e67b50
Adam Miller <theadammiller56@gmail.com>
2020-06-19 11:37:33 -0500

    BootStraps Website additions

---
+++ CTWMasterClass-FrontEndActivities/CTWMasterClass-FrontEndActivities/Miller/JSSite.js
@@ -0,0 +1,11 @@
+﻿
+function showHide() {
+    var x = document.getElementById("vision");
+    if (x.style.display !== "none") {
+        x.style.display = "none";
+    }
+    else {
+        x.style.display = "block";
+    }
+}
+
